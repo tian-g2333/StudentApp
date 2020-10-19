3023f0730d95e0a476b3bc6f8f497a3a643f9619
tian-g2333 <1129327898@qq.com>
2020-10-19 23:12:51 +0800

    快递

---
+++ app/src/main/java/com/android/hys/studentapp/KuaidiService.java
@@ -0,0 +1,78 @@
+package com.android.hys.studentapp;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class KuaidiService {
+    public static boolean kuaidiSelect(String name, String info) {
+        KuaidiThread kuaidiThread = new KuaidiThread("http://8.129.48.153:8080/MyWeb/SignIn",name,info);
+        try
+        {
+            kuaidiThread.start();
+            kuaidiThread.join();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+
+        return kuaidiThread.getResult();
+    }
+
+    public static boolean kuaidiAdd(String name, String info) {
+        KuaidiThread kuaidiThread = new KuaidiThread("http://8.129.48.153:8080/MyWeb/DeliveryAdd",name,info);
+        try
+        {
+            kuaidiThread.start();
+            kuaidiThread.join();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        return kuaidiThread.getResult();
+    }
+}
+
+class KuaidiThread extends Thread
+{
+    private String path;
+    private String name;
+    private String info;
+    private boolean result = false;
+
+    public KuaidiThread(String path,String name,String info)
+    {
+        this.path = path;
+        this.name = name;
+        this.info = info;
+    }
+    @Override
+    public void run()
+    {
+        try {
+            URL url = new URL(path);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setConnectTimeout(8000);//设置连接超时时间
+            httpURLConnection.setReadTimeout(8000);//设置读取超时时间
+            httpURLConnection.setRequestMethod("POST");//设置请求方法,post
+
+            String data = "name=" + URLEncoder.encode(name, "utf-8") + "&info=" + URLEncoder.encode(info, "utf-8");//设置数据
+            httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");//设置响应类型
+            httpURLConnection.setRequestProperty("Content-Length", data.length() + "");//设置内容长度
+            httpURLConnection.setDoOutput(true);//允许输出
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            outputStream.write(data.getBytes("utf-8"));//写入数据
+            result = (httpURLConnection.getResponseCode() == 200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getResult()
+    {
+        return result;
+    }
+}
--- app/src/main/java/com/android/hys/studentapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/android/hys/studentapp/ui/home/HomeFragment.java
@@ -1,10 +1,14 @@
 package com.android.hys.studentapp.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +16,11 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.hys.studentapp.Activity_home;
+import com.android.hys.studentapp.KuaidiService;
+import com.android.hys.studentapp.MainActivity;
 import com.android.hys.studentapp.R;
+import com.android.hys.studentapp.UserService;
 
 public class HomeFragment extends Fragment {
 
@@ -24,5 +32,37 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final EditText et_name = (EditText) getActivity().findViewById(R.id.et_name);
+        final EditText et_info = (EditText) getActivity().findViewById(R.id.et_info);
+        Button add=(Button) getActivity().findViewById(R.id.ensure_add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = et_name.getText().toString();
+                String info = et_info.getText().toString();
+
+                if (KuaidiService.kuaidiAdd(name, info))
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(), "提交成功", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                else {
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(), "提交失败", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            }
+        });
     }
 }
